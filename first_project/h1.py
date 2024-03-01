3897c590dd90303408904909ca58e36a87170192
tugba0105 <tugbacan0105@gmail.com>
2024-03-01 09:57:49 +0000

    Create h1.py

---
+++ first_project/h1.py
@@ -0,0 +1,24 @@
+print("hello world!")
+x=3
+
+y=5
+y= "john"
+print("x=", x)
+print("y=", y)
+print(type(x))
+print(type(y))
+
+Y="case-sensitive"
+print(x,y,Y)
+
+#x = str(3)
+#y = int(3)
+#z = float(3)
+
+#print(x,y,Y)
+
+
+
+
+
+
